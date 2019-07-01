9a4d0dda197322485858997c55ef8108867f6ae5
Tewson Seeoun <tewson.seeoun@toasttab.com>
2019-07-01 20:20:56 +0100

    Add a simple React app

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: "babel-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    })
+  ]
+};
