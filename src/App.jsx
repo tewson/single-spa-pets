3ca8ed410bb3b11ca52c25f508a13b23e6b9d193
Tewson Seeoun <tewson.seeoun@toasttab.com>
2019-07-02 10:07:03 +0100

    Add react-router and sample routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-export default () => <h1>Pets!</h1>;
+export default () => (
+  <Router>
+    <h1>Pets!</h1>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cat">Cat</Link>
+        </li>
+        <li>
+          <Link to="/dog">Dog</Link>
+        </li>
+      </ul>
+    </nav>
+    <Route exact path="/" component={() => <h2>Welcome to Pets!</h2>} />
+    <Route exact path="/cat" component={() => "Cat"} />
+    <Route exact path="/dog" component={() => "Dog"} />
+  </Router>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "src/index.html"
     })
-  ]
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
 };
